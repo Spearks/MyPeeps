84545ab3fbfbc226a2f52460e8e1cbced2d0121e
Spearks <otavio.canuto.m@gmail.com>
2023-10-07 22:50:56 -0300

    ADD: Actions throttle-limited

--- api/views/PeepsActions.py
+++ api/views/PeepsActions.py
@@ -9,12 +9,16 @@ from api.serializers import ActionSerializer, PeepsSerializer
 from api.permissions import PeepsUserPermissions
 
 from mypeeps.settings import ACTIONS
+
+from django.utils import timezone
+from datetime import timedelta
+
 actions = ACTIONS
 
 def apply_effect(UserOption):
     pass
 
-# TODO: User auth, Rate-limit, history 
+# TODO: Split in functions to be more readable
 @extend_schema(
     request=ActionSerializer, 
     responses={200: {'description': 'Action successful'}}, 
@@ -31,20 +35,32 @@ class ActionsPeepView(APIView):
             selected_action = actions[serializer.validated_data["name"]]
             peep = serializer.validated_data["peep"]
             useroption = selected_action["UserOptions"][serializer.validated_data["options"]["name"]]
+                
+
+            current_time = timezone.now()
+            # Check if Peep is in throttling 
+            last_metric = PeepsMetric.objects.all().cache().filter(peep=peep, action=serializer.validated_data["options"]["name"]).last()
+        
+            rate_limit_threshold = current_time - timedelta(minutes=selected_action["Ratelimit"]["minute"])
+
+            if last_metric.time > rate_limit_threshold:
+                message = {
+                    "message" : "Action are in throttle-limited"
+                }
+                return Response(message)
             
             peep.add_to_attribute("romance", +2.2)
             peep.save()
 
-
+            # Save to metrics
             metric = PeepsMetric()
-            metric.action = serializer.validated_data["options"]["name"] # 
+            metric.action = serializer.validated_data["options"]["name"] 
             metric.peep = peep
             metric.attribute_hp = peep.attribute_hp
             metric.attribute_creativity = peep.attribute_creativity
             metric.attribute_romance = peep.attribute_romance
 
             metric.save()
-
 
             peep_serialized = PeepsSerializer(peep) 
 
