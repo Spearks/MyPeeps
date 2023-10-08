d82fb8253bbed516d81c8737c17078f48f3b6440
Spearks <otavio.canuto.m@gmail.com>
2023-10-08 20:59:56 -0300

    ADD: Tests on Docker. Whitenoise support

--- runner.sh
+++ runner.sh
@@ -5,5 +5,6 @@ export $(grep -v '^#' .env | xargs)
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+python manage.py test
 
 gunicorn mypeeps.wsgi -b 0.0.0.0
